e9acb455c25c548cebeb6d6188b2eadca5b1c663
KaiSteven <1074699390@QQ.com>
2019-01-11 22:32:29 +0800

    第三次提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,29 +1,63 @@
+const app = getApp();
+
 Page({
 
-  data:{
-    longitude:"",
-    latitude:""
+  data: {
+    longitude: "",
+    latitude: "",
+    //红点小图标
+    controls: [{
+        iconPath: '/images/pin.png',
+        position: {
+          left: (app.globalData.windowWidth / 2) - 11,
+          top: ((app.globalData.windowHeight - 60) / 2) - 31,
+          width: 22,
+          height: 31
+        }
+      },
+      {
+        id: 1,
+        iconPath: '/images/center.png',
+        position: {
+          left: 25,
+          top: (app.globalData.windowHeight - 125),
+          width: 30,
+          height: 30,
+        },
+        clickable: true
+      }
+    ]
 
   },
 
-  onShow (){
+  onShow() {
     this.getLocation();
   },
-  getLocation(){
+
+  //点击回到中心位置
+  onReady() {
+    this.mapCtx = wx.createMapContext('map');
+  },
+
+  //获取当前地理位置
+  getLocation() {
     wx.getLocation({
       type: 'gcj02',
       success: this.handleGetLocationSucc.bind(this)
     })
   },
-  handleGetLocationSucc(res){
+  handleGetLocationSucc(res) {
     this.setData({
-      longitude:res.longitude,
+      longitude: res.longitude,
       latitude: res.latitude
 
     })
 
   },
 
+  controltap() {
+    this.mapCtx.moveToLocation();
+  },
 
   onShareAppMessage(res) {
     if (res.from === 'button') {
@@ -34,5 +68,6 @@ Page({
       title: '萌宠交易平台',
       path: '/pages/index/index'
     }
-  }
+  },
+
 })
--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -1,4 +1,4 @@
-//logs.js
+
 const util = require('../../utils/util.js')
 
 Page({
